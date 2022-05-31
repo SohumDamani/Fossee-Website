7e9075bbf637504271a34ca62004c64a7f6610b6
SohumDamani <damanisohum@gmail.com>
2022-05-31 14:11:00 +0530

    Added Navbar and banner data to layout page

---
+++ Website/Project/Project/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-let6*!@^-0hx6r_bl#$_o8hi%bvtzybti%^@yuvg(g#pfid1of"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ Website/Project/streams/blocks.py
@@ -0,0 +1,56 @@
+from wagtail import blocks
+
+
+
+class LinkBlock(blocks.StructBlock):
+    link = blocks.RichTextBlock(features=['link'], required=True)
+    visible = blocks.BooleanBlock(required=False, default=True)
+
+    class Meta:
+        template="streams/self_block.html"
+        icon = 'link'
+        label = 'Link'
+
+class Gallery(blocks.ListBlock):
+
+    class Meta:
+        icon='edit'
+
+class StreamBlock(blocks.StreamBlock):
+    pass
+
+class CharBlock(blocks.CharBlock):
+    pass
+
+class TitleAndContent(blocks.StructBlock):
+    title = blocks.CharBlock(max_length=250,required=True)
+    content = blocks.RichTextBlock(required=True)
+
+    class Meta:
+        icon='edit'
+
+
+class SidebarBlock(blocks.StructBlock):
+    title = blocks.CharBlock(max_length=250,required=True)
+    links = StreamBlock(
+        [
+            ('link',LinkBlock())
+        ]
+    )
+
+    class Meta:
+        template = "streams/self_block.html"
+        icon = 'edit'
+        label = 'Content'
+
+class FooterBlock(blocks.StructBlock):
+    title = CharBlock()
+    content = blocks.RichTextBlock(label='Message')
+
+    class Meta:
+        template = "streams/self_block.html"
+        icon='edit'
+        label = 'Content'
+
+
+
